363e8688b683ed2fee8a8a4c1941a93240c0f29a
Spencer James <spencer@gitwit.com>
2021-02-19 08:13:48 -0600

    Updating for v0 of epsilon-greedy

--- application.py
+++ application.py
@@ -5,7 +5,12 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 
-st.write("Hey There")
+st.header("Epsilon Greedy v0")
+st.subheader("Proof of concept for reinforcement learning algorithm")
+st.subheader("AKA - The Multi-Armed Bandit")
+st.write('Clicking the "Compute" button below will simulate the multi-armed bandit problem.  What is the multi-armed bandit problem?')
+st.write('Imagine you are in a casino and walk up to ten identical slot machines that say "Play for Free!".  Each machine has a different average payout between $0 and $10.  Naturally, you would want to play whichever machine has the highest average payout, but how do you know which one that is?')
+st.write('See this article for a more thorough description: ', 'https://www.datacamp.com/community/tutorials/introduction-reinforcement-learning')
 
 # initialize a random seed
 np.random.seed(5)
@@ -52,49 +57,50 @@ def bestArm(a):
     return bestArm
 
 
-# Here is the main loop.  Let's play it 500 times and display a matplotlib scatter plot 
-# of the mean reward against the number of times the game is played.
-plt.xlabel("Number of times played")
-plt.ylabel("Average Reward")
-column_names = ["Number of times played", "Average Reward"]
-df = pd.DataFrame(columns = column_names)
-for i in range(200):
-    if random.random() > eps: #greedy exploitation action
-        choice = bestArm(av)
-        thisAV = np.array([[choice, reward(arms[choice])]])
-        av = np.concatenate((av, thisAV), axis=0)
-    else: #exploration action
-        choice = np.where(arms == np.random.choice(arms))[0][0]
-        thisAV = np.array([[choice, reward(arms[choice])]]) #choice, reward
-        av = np.concatenate((av, thisAV), axis=0) #add to our action-value memory array
-    #calculate the mean reward
-    runningMean = np.mean(av[:,1])
-    #st.write(i, runningMean)
-    #st.write(df)
-    dftmp = pd.DataFrame([{"Number of times played": i, "Average Reward": runningMean, "animation": i}])
-    #st.write(dftmp)
-    df = df.append(dftmp)
-    #st.write(df)
-    #fig, ax1 = plt.subplots() #solved by add this line
-    #if i == 0:
-    #    ax0 = plt.scatter(i, runningMean, ax=ax1)
-    #else:
-    #    ax1 = plt.scatter(i, runningMean, ax=ax1)
-    
-    #st.pyplot(fig)
-    #if i ==1 :
-    #	fig1 = px.scatter(df, x="Number of times played", y="Average Reward", range_x=[0,500], range_y=[0,10])
-    #else:
-    #	fig2 = px.scatter(df, x="Number of times played", y="Average Reward", range_x=[0,500], range_y=[0,10])
-    #fig3 = go.Figure(data=fig1.data + fig2.data)
-    #st.plotly_chart(fig3)
-st.button('Compute')
-fig = px.scatter(df, x="Number of times played", y="Average Reward", range_x=[0,200], range_y=[0,10])
-st.plotly_chart(fig)
 
-st.write('See it in motion!')
-fig = px.scatter(df, x="Number of times played", y="Average Reward", animation_frame='animation', range_x=[0,200], range_y=[0,10])
-#fig = px.line(df, x="Number of times played", y="Average Reward", animation_frame='animation', range_x=[0,500], range_y=[0,10])
-st.plotly_chart(fig)
-    
-    
+
+if st.button('Compute'):
+	st.write("Let's Gamble")
+	st.write("Playing the multi-armed bandit 200 times real quick.  Check out how my average payout increases each time I play because I'm learning which patterns work and get smarter each time!")
+	# Here is the main loop.  Let's play it 500 times and display a matplotlib scatter plot 
+	# of the mean reward against the number of times the game is played.
+	plt.xlabel("Number of times played")
+	plt.ylabel("Average Reward")
+	column_names = ["Number of times played", "Average Reward"]
+	df = pd.DataFrame(columns = column_names)
+	for i in range(200):
+		if random.random() > eps: #greedy exploitation action
+			choice = bestArm(av)
+			thisAV = np.array([[choice, reward(arms[choice])]])
+			av = np.concatenate((av, thisAV), axis=0)
+		else: #exploration action
+			choice = np.where(arms == np.random.choice(arms))[0][0]
+			thisAV = np.array([[choice, reward(arms[choice])]]) #choice, reward
+			av = np.concatenate((av, thisAV), axis=0) #add to our action-value memory array
+		#calculate the mean reward
+		runningMean = np.mean(av[:,1])
+		#st.write(i, runningMean)
+		#st.write(df)
+		dftmp = pd.DataFrame([{"Number of times played": i, "Average Reward": runningMean, "animation": i}])
+		#st.write(dftmp)
+		df = df.append(dftmp)
+	fig = px.scatter(df, x="Number of times played", y="Average Reward", range_x=[0,200], range_y=[0,10])
+	fig.update_traces(marker=dict(size=16,
+	                              line=dict(width=2,
+	                                        color='DarkSlateGrey')),
+	                  selector=dict(mode='markers+lines'))
+	st.plotly_chart(fig)
+	st.write("Try it again. Since you start from 0 each time, notice how the first 50 turns or so are different, but with enough plays and cumulative learnings the average payout will always increase.")
+
+	st.write('See it in motion!')
+	fig = px.scatter(df, x="Number of times played", y="Average Reward", animation_frame='animation', range_x=[0,100], range_y=[0,10])
+	fig.update_traces(marker=dict(size=16,
+	                              line=dict(width=2,
+	                                        color='DarkSlateGrey')),
+	                  selector=dict(mode='markers'))
+	fig.update_traces(line=dict(dash="dot", width=2, color='DarkSlateGrey'), selector=dict(type='scatter', mode='lines'))
+
+	#fig = px.line(df, x="Number of times played", y="Average Reward", animation_frame='animation', range_x=[0,500], range_y=[0,10])
+	st.plotly_chart(fig)
+
+
