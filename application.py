fa9253e54ab25864fa3adbaf8c907f8d326d20a1
Spencer James <spencer@gitwit.com>
2021-02-20 13:39:07 -0600

    added v0.3 in which the reward is continuously update

--- application.py
+++ application.py
@@ -6,7 +6,135 @@ import plotly.express as px
 import pandas as pd
 
 
-report = st.sidebar.selectbox("Epsilon Greedy Model",["Epsilon Greedy v0", "Epsilon Greedy v0.1", "Epsilon Greedy v0.2"])
+report = st.sidebar.selectbox("Epsilon Greedy Model",["Epsilon Greedy v0", "Epsilon Greedy v0.1", "Epsilon Greedy v0.2", "Epsilon Greedy v0.3"])
+
+if report == "Epsilon Greedy v0.3":
+	st.header("Epsilon Greedy v0.3")
+	st.subheader("4th Proof of concept for reinforcement learning algorithm")
+	st.subheader("AKA - The small variations on a well performing curve that dynamically updates the definition of the well performing curve")
+	st.write('Clicking the "Compute" button below will initialize a curve assuming a benchmark conversion rate. It will then update the winning conversion rate after every check in for 1000 patient check-in attempts.')
+	st.write('For Epsilon Greedy v0.3, we aren\'t actually going to specify what the well performing curve is.  We will just start with a conversion rate of 30% and assume that randomly changes about 10% of the time in a good or bad direction.  Over time we expect this to continue to increase - we will see...')
+
+	# initialize a random seed
+	np.random.seed(5)
+
+	# Set number of arms for the bandit problem
+	n=1
+
+	curves = [0.318]
+
+	#generate the distribution of curves and associated probabilities.
+	#mu, sigma = 0.318, 0.035
+
+	#curves = np.random.normal(mu, sigma, n)
+
+	# do some quick sanity checks and plots
+	#verify the mean
+	#mean = np.mean(curves)
+	#st.write('mean of conversion rates = ', mean)
+
+	#verify the variance
+	#std = np.std(curves, ddof=1)
+	#st.write('standard deviation of conversion rates = ', std)
+
+	# plot histogram of conversion rates in this model
+	#count, bins, ignored = plt.hist(curves, 30, density=True)
+	#fig, ax = plt.subplots() 
+	#ax = plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) *
+    #           np.exp( - (bins - mu)**2 / (2 * sigma**2) ),
+    #     linewidth=2, color='r')
+	#st.pyplot(fig)
+
+
+	# Equate arms with known curve conversion rates from this google slide: https://docs.google.com/presentation/d/1yIwNGm6Q8MbohnkOEP_ww6gRrgSytVFTxdZQ2izAjcU/edit#slide=id.gbd766acf5e_1_0
+	arms = curves
+
+	# Set the probability of exploration, epsilon
+	eps = 0.1
+
+
+	# for every arm, loop through ten iterations and generate
+	# a random number each time.  If this random number is less
+	# than the probability of that arm, we add 1 to the reward.
+	# after all 10 iterations we'll have a value between 1 and 10.
+	def reward(prob):
+		reward = 0
+
+		for i in range(1):
+			if random.random() < prob:
+				reward += 1
+
+		return reward
+
+
+	# initialize memory array; has 1 row defaulted to random action index
+	av = np.array([np.random.randint(0,(n+1)), 0]).reshape(1,2) #av = action-value
+
+	# greedy method to select best arm based on memory array
+	# This function accepts a memory array that stores the history of all actions and their rewards. 
+	# It is a 2 x k matrix where each row is an index reference to your arms array (1st element), and the reward received (2nd element). 
+	# For example, if a row in your memory array is [2, 8], it means that action 2 was taken (the 3rd element in our arms array) 
+	# and you received a reward of 8 for taking that action.
+	def bestArm(a):
+	    bestArm = 0 #default to 0
+	    bestMean = 0
+	    for u in a:
+	        avg = np.mean(a[np.where(a[:,0] == u[0])][:, 1]) #calculate mean reward for each action
+	        if bestMean < avg:
+	            bestMean = avg
+	            bestArm = u[0]
+	    return bestArm
+
+
+
+
+	if st.button('Compute'):
+		st.write("Let's Gamble")
+		st.write("Simulating 2000 patients using arriv real quick.")
+		# Here is the main loop.  Let's play it 500 times and display a matplotlib scatter plot 
+		# of the mean reward against the number of times the game is played.
+		plt.xlabel("Number of patients simulated")
+		plt.ylabel("Average Conversion Rate")
+		column_names = ["Number of patients simulated", "Average Conversion Rate"]
+		df = pd.DataFrame(columns = column_names)
+		for i in range(2000):
+			if random.random() > eps: #greedy exploitation action
+				choice = bestArm(av)
+				thisAV = np.array([[choice, reward(arms[choice])]])
+				av = np.concatenate((av, thisAV), axis=0)
+			else: #exploration action
+			# This is the key step in which we vary the available conversion rate by a fixed amount
+				choice = np.where(arms == np.random.choice(arms))[0][0]
+				arms[choice] = arms[choice]+0.05*(random.random() - 0.5)
+				#st.write(arms[choice])
+				thisAV = np.array([[choice, reward(arms[choice])]]) #choice, reward
+				av = np.concatenate((av, thisAV), axis=0) #add to our action-value memory array
+				
+			#calculate the mean reward
+			runningMean = np.mean(av[:,1])
+			#st.write(i, runningMean)
+			#st.write(df)
+			dftmp = pd.DataFrame([{"Number patients simulated": i, "Average Conversion Rate": runningMean, "animation": i}])
+			#st.write(dftmp)
+			df = df.append(dftmp)
+		fig = px.scatter(df, x="Number patients simulated", y="Average Conversion Rate", range_x=[0,2000], range_y=[0,1])
+		fig.update_traces(marker=dict(size=16,
+		                              line=dict(width=2,
+		                                        color='DarkSlateGrey')),
+		                  selector=dict(mode='markers+lines'))
+		st.plotly_chart(fig)
+		st.write("Try it again. Since you start with no a-priori knowledge each time, notice how the average conversion rate varies highly in the beginning and then settles into a steady state after enough learnings.")
+		#st.write('See it in motion!')
+		#fig = px.scatter(df, x="Number of times played", y="Average Reward", animation_frame='animation', range_x=[0,200], range_y=[0,1])
+		#fig.update_traces(marker=dict(size=16,
+		#                              line=dict(width=2,
+		#                                        color='DarkSlateGrey')),
+		#                  selector=dict(mode='markers'))
+		#fig.update_traces(line=dict(dash="dot", width=2, color='DarkSlateGrey'), selector=dict(type='scatter', mode='lines'))
+
+		#fig = px.line(df, x="Number of times played", y="Average Reward", animation_frame='animation', range_x=[0,500], range_y=[0,10])
+		#st.plotly_chart(fig)
+
 
 if report == "Epsilon Greedy v0.2":
 	st.header("Epsilon Greedy v0.2")
